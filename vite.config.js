29039453e193c32970516f289a171ddde05e9fd1
Cleber Vasconcelos <crebs.vas@gmail.com>
2023-03-13 23:11:47 -0300

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pokemon-battle/',
+  base: './',
+  build: {
+    outDir: 'build/',
+  },
 });
